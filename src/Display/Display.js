4b9ff75d3cdf82889905d2ad27025675991e954d
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-09-21 14:26:15 +0100

    Created application displaying images and includes a form

---
+++ src/Display/Display.js
@@ -0,0 +1,40 @@
+import React from "react";
+import classes from "./Display.module.css";
+import Form from "../Form/Form";
+
+const Display = (props) => {
+  return (
+    <div>
+      <div>
+        <div className={classes.container}>
+          {props.images &&
+            props.images.map((img) => (
+              <div className={classes.galleryContainer}>
+                <div className={classes.galleryItem}>
+                  <div className={classes.image} key={img.id}>
+                    <img src={img.url} alt={img.alt_description} />
+                  </div>
+                  <div className={classes.text}>{img.user.first_name}</div>
+                </div>
+              </div>
+            ))}
+
+          {props.images &&
+            props.images.map((img) => (
+              <div className={classes.galleryContainer}>
+                <div className={classes.galleryItem}>
+                  <div className={classes.image} key={img.id}>
+                    <img src={img.user.profile_image} alt="" />
+                  </div>
+                  <div className={classes.text}>{img.user.first_name}</div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+      <Form />
+    </div>
+  );
+};
+
+export default Display;
---
+++ src/Form/Form.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import classes from "./Form.module.css";
+
+const Form = () => {
+  const [value, setValue] = useState(50000);
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <div className={classes.container}>
+        <form>
+          <div className={classes.formItem}>
+            <div className={classes.text}>Name</div>
+            <input type="text" required></input>
+          </div>
+          <div className={classes.formItem}>
+            <div className={classes.text}>Email</div>
+            <input type="email" required></input>
+          </div>
+          <div className={classes.formItem}>
+            <div className={classes.text}>Date of Birth</div>
+            <input type="date" required></input>
+          </div>
+          <div className={classes.formItem}>
+            <div className={classes.text}>Favourite Colour</div>
+            <input type="color"></input>
+          </div>
+          <div className={classes.formItem}>
+            <div className={classes.text}>Salary</div>
+            <div className={classes.salary}>
+              <input
+                type="range"
+                min="0"
+                max="100000"
+                id="slider"
+                onChange={handleChange}
+                value={value}
+              ></input>
+
+              <p>£{value}</p>
+            </div>
+          </div>
+          <button className={classes.btn}>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
